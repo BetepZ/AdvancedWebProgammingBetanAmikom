2ddc56b39a193bc30d3fda8d03eba93018a89e49
BetepZ <146155762+BetepZ@users.noreply.github.com>
2025-10-05 15:49:07 +0700

    Initial commit

---
+++ aboutme_24.62.0221.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header-1">
+            <?php
+            echo "<h1> Header Betan </h1>";
+            ?>
+        </div>
+        <div class="main">
+            <div class="main-left">
+                <?php
+                echo "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1'>My Instagram</a><br>";
+                echo "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1'>My Facebook</a><br>";
+                echo "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1'>My Reddit</a><br>";
+                echo "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1'>My OnlyFun</a>";
+                ?>
+
+            </div>
+
+            <div class="main-right">
+                <?php
+                echo "<h2>About Me</h2><br>";
+                echo "<p>Nama : Betano Putra Fortuna </p><br>";
+                echo "<p>Hobi Gweh : Ngoding PHP </p><br>";
+                echo "<p>Cita-Cita : Nikah dengan Mahiru Shina </p><br>";
+                echo "<p>Nickname : AwP_Shotieng </p><br>";
+                ?>
+            </div>
+
+        </div>
+
+        <div class="footer">
+            <?php
+            echo "<p>Nama : Betano Putra Fortuna | NIM : 24.62.0221 | Kelas : BCIS 24 </p>";
+            ?>
+        </div>
+    </div>
+
+</body>
+
+</html>
